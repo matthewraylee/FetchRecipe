30d1d6b45c2c4f9be98aa0cc2bb608da538b0397
Matthew Ray Lee <matthewraylee@gmail.com>
2023-11-06 20:24:56 -0800

    Started Project in Xcode

---
+++ FetchRecipe/FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Matthew Lee on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
